630835380e547fdddf39ec5365dc28d8a698de16
DawidSieczka <dawid.sieczka.praca@gmail.com>
2023-09-28 21:12:05 +0200

    Init project

---
+++ MAWService/MAWService.Api/Controllers/BaseApi.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAWService.Api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class BaseApi : ControllerBase
+{
+    public BaseApi()
+    {
+        
+    }
+}
